22078bdb10154fb84d11cf9a6629908322e1b5bf
cuixing158 <1229261804@qq.com>
2024-09-06 10:28:29 +0800

    chore:add java dependency

---
+++ app/src/main/java/com/example/my360panorama/IJKPlayerTestActivity.java
@@ -0,0 +1,92 @@
+package com.example.my360panorama;
+
+import android.graphics.SurfaceTexture;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import tv.danmaku.ijk.media.player.IMediaPlayer;
+import tv.danmaku.ijk.media.player.IjkMediaPlayer;
+
+public class IJKPlayerTestActivity extends AppCompatActivity {
+
+    private static final String TAG = "bz_IJKPlayerTest";
+    private TextureView texture_view;
+    private Surface surface;
+    private IjkMediaPlayer ijkMediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ijkplayer_test);
+        texture_view = findViewById(R.id.texture_view);
+        texture_view.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+                IJKPlayerTestActivity.this.surface = new Surface(surfaceTexture);
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+            }
+        });
+    }
+
+    public void startPlay(View view) {
+        if (null == surface) {
+            Log.d(TAG, "null == surface");
+            return;
+        }
+        if (null == ijkMediaPlayer) {
+            ijkMediaPlayer = new IjkMediaPlayer();
+        } else {
+            ijkMediaPlayer.reset();
+        }
+        try {
+            ijkMediaPlayer.setSurface(surface);
+            ijkMediaPlayer.setDataSource(getFilesDir().getAbsolutePath()+"/360panorama.mp4"); // 这是是视频文件路径
+            ijkMediaPlayer.setOnPreparedListener(new IMediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(IMediaPlayer iMediaPlayer) {
+                    iMediaPlayer.start();
+                }
+            });
+            ijkMediaPlayer.prepareAsync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (null != ijkMediaPlayer) {
+            ijkMediaPlayer.pause();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != ijkMediaPlayer) {
+            ijkMediaPlayer.stop();
+            ijkMediaPlayer.release();
+        }
+    }
+}
